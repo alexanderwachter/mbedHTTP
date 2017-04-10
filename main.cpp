6e9ed34c6cba0c84dee53f543ec73312b2d05f10
Alexander Wachter <alexander.wachter@student.tugraz.at>
2017-04-10 20:57:36 +0200

    ADD: put/get/delete data demo

--- main.cpp
+++ main.cpp
@@ -65,7 +65,47 @@ void TestHandler::doDelete(HTTPRequest& request, HTTPResponse& response)
   response.setRespCode(HTTP_MethodNotAllowed);
 }
 
+char _message[30];
+
+class DataHandler : public HTTPRequestHandler
+{
+public:
+  DataHandler(){}
+  virtual ~DataHandler(){}
+  void doGet(HTTPRequest&, HTTPResponse&);
+  void doPost(HTTPRequest&, HTTPResponse&);
+  void doPut(HTTPRequest& request, HTTPResponse& response){ response.setRespCode(HTTP_MethodNotAllowed); };
+  void doDelete(HTTPRequest&, HTTPResponse&);
+  int _data;
+private:
+};
+
+void DataHandler::doGet(HTTPRequest& request, HTTPResponse& response)
+{
+  string msg;
+  msg = to_string(_data);
+  msg += "unit";
+  strcpy(_message, msg.c_str());
+  response.setData(_message);
+};
+void DataHandler::doPost(HTTPRequest& request, HTTPResponse& response)
+{
+  auto data_it = request._request_data.find("data");
+  if(data_it == request._request_data.end())
+  {
+    response.setRespCode(HTTP_NotModified);
+    return;
+  }
+  _data = atoi(data_it->second.c_str());
+}
+
+void DataHandler::doDelete(HTTPRequest& request, HTTPResponse& response)
+{
+  _data = 0;
+}
+
 TestHandler test_h;
+DataHandler data_h;
 DigitalOut led1(LED1);
 
 int main() 
@@ -82,6 +122,7 @@ int main()
   HTTPServer http_server(&net);
   http_server.runThread();
   http_server.addHandler(&test_h, "/test.html");
+  http_server.addHandler(&data_h, "/data");
   while(true)
   {
     led1 = !led1;
